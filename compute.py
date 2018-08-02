6675802b76842a2c0a5ae6a9372930c8198a6910
loqoman <darwin.s.clark@gmail.com>
2018-08-02 12:19:40 -0700

    Many changed. Added support to import graph generation scripts. Added argument parsing support. Added logger functionality

--- compute.py
+++ compute.py
@@ -1,7 +1,7 @@
-# 
-
 from os.path import abspath, dirname, exists, join
 from optparse import OptionParser
+
+from argparse import ArgumentParser
 
 import os
 import logging
@@ -9,38 +9,78 @@ import functools
 
 import webbrowser
 
+# Nessissary to import files based on a string
+import importlib
+
+# Nessissary to import files lower in the directory chain
+import sys
+
+logger = logging.getLogger('4915DataAnalysis')
 if __name__ == '__main__':
 
-    parser = OptionParser()
+    optParser = OptionParser()
 
+    argParser = ArgumentParser()
     # Foreward: Parsing arguments is a very powerful tool.
     # It may be a good idea to return to this, and find potentially more efficent ways to do this
-    parser.add_option('-v', '--verbose', default=False, action='store_true',
+    optParser.add_option('-v', '--verbose', default=False, action='store_true',
                     help='Enable verbose logging')
 
     
     # The 'action' field revolves around what should be done when '-d' is entered in the terminal. 
     # In this case, it is told to 'store_true', which is self explanitory
-    parser.add_option('-d', '--display', default=False, action='store_true',
+    optParser.add_option('-d', '--display', default=False, action='store_true',
                     help='Displays the generated graph in the default image viewer')
 
     
-    parser.add_option('--delete',
+    optParser.add_option('--delete',default=True, action='store_false',
                     help='Deletes the generated graph after it is displayed')
 
-    options, args = parser.parse_args()
-
-    print(options)
+    # Can add support for parsing multiple files
+    # nArgs > 1
+    argParser.add_argument('file',nargs='?')
     
-    # Note: With an action of 'store_true', the true or false is not put into the args array
-    print(args)
+    options, args = optParser.parse_args()
 
-    if options.display:
-        webbrowser.open('Out/Graph number 7.png')
+    log_datefmt = "%H:%M:%S"
+    log_format = "%(asctime)s %(levelname)-6s: %(name)-8s: %(message)s"
+
+    logging.basicConfig(datefmt=log_datefmt,
+                        format=log_format,
+                        level=logging.DEBUG if options.verbose else logging.INFO)
+    
+    sys.path.append('./Python')
+
+    logger.info("Initilized %(file)s with the following options: %(options)s",dict(file=__file__,options=options))
+
+    logger.debug("Current sys.path: %s",sys.path)
+    # Support for multiple files
+    imported_modules = []
+    
+    # For every argument given
+    for argument in args:
+        
+        # List all the scripts in the 'python' directory
+        scripts = os.listdir('Python')
+        logger.info("Searching for %s in /Python...",argument)
+        
+        # For all the scripts(strings ATM)
+        for fileNames  in scripts:
+            
+            # If the Script name(string) is the same as an argumen tgiven
+            if argument == fileNames:
+                
+                # Log it
+                logger.info("Found %s in /Python...",argument)
+                
+                
+                # Import it(But remove .py from the end of it)   
+                imported_modules.append(__import__(argument[:-3]))
 
 
-
-# Code for image deletion
+         
+            
+    
 '''
 import os
 os.remove(file) for file in os.listdir('path/to/directory') if file.endswith('.png')
